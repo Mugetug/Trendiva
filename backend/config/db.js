7f43122db412f51f1e8529b33da1332b3300b26f
Mugetug <mugetug03@gmail.com>
2026-04-08 02:28:08 +0300

    db connection fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,34 @@
+//const mongoose = require("mongoose");
+
+//const connectDB = async () => {
+  //try {
+    //await mongoose.connect(process.env.MONGO_URI);
+    //console.log("MongoDB bağlantısı başarılı");
+  //} catch (error) {
+    //console.error("MongoDB bağlantı hatası:", error.message);
+    //process.exit(1);
+  //}
+//};
+
+//module.exports = connectDB;
+
+
+
+
+
+
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB bağlantısı başarılı");
+    // Render değişkenine güvenmek yerine linki doğrudan buraya yazıyoruz
+    const connectionString = "mongodb+srv://trendivaUser:Trendiva123@cluster0.raulpkm.mongodb.net/trendivaDB?retryWrites=true&w=majority";
+
+    await mongoose.connect(connectionString);
+    console.log("-----------------------------------------");
+    console.log("MONGODB BAĞLANTISI BAŞARILI - SİSTEM AKTİF");
+    console.log("-----------------------------------------");
   } catch (error) {
     console.error("MongoDB bağlantı hatası:", error.message);
     process.exit(1);
